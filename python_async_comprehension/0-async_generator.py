8dee056ace0d704e5156d226a3139f68600a661e
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-11-28 14:41:57 +0100

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Yield 10 random numbers between 0 and 10"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yield 10 random numbers between 0 and 10, waiting 1 second between each."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
