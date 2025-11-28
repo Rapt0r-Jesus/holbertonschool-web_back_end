628222af274ab59005d83473ba3b9ad1b651780f
Rapt0r-Jesus <dorian.oufer@gmail.com>
2025-11-28 15:05:19 +0100

    correction task 0 et 01

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,14 @@
 """Yield 10 random numbers between 0 and 10"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """Yield 10 random numbers between 0 and 10, waiting 1 second between each."""
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Yield a random number between 0 and 10,
+    10 times, with a 1-second delay.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""doctrsing for task 01"""
 from typing import List
-
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collect 10 random numbers from async_generator using async comprehension."""
-    return [i async for i in async_generator()]
+    """
+    Collect 10 random numbers from
+    sync_generator using async comprehension.
+    """
+    return [num async for num in async_generator()]
